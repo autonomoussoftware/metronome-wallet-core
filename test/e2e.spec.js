e5be1b45de8b6dc63b36df5c1288a45213b24db8
Gabriel Montes <gabriel@bloq.com>
2019-11-01 18:15:27 -0300

    Add tests to check wallet events

--- test/e2e.spec.js
+++ test/e2e.spec.js
@@ -1,10 +1,18 @@
 'use strict'
 
 const { once } = require('lodash')
+const bip39 = require('bip39')
+const createDebug = require('debug')
+const util = require('util')
+
 require('chai').should()
 require('dotenv').config()
 
 const createCore = require('..')
+
+createDebug.formatters.J = obj =>
+  util.inspect(obj, { colors: true, depth: 4, sorted: true })
+const debug = createDebug('metronome-wallet:core:test:e2e')
 
 describe('Core E2E', function () {
   before(function () {
@@ -99,6 +107,83 @@ describe('Core E2E', function () {
 
       emitter.emit('open-wallets', { activeWallet: walletId, address })
     })
+
+    it('should send ETH and emit a wallet event', function (done) {
+      this.timeout(0)
+
+      const core = createCore()
+      const config = {
+        indexerUrl: process.env.ROPSTEN_INDEXER,
+        wsApiUrl: process.env.ROPSTEN_NODE
+      }
+      const { api, emitter } = core.start(config)
+
+      const mnemonic = process.env.MNEMONIC
+      const seed = bip39.mnemonicToSeedHex(mnemonic).toString('hex')
+      const address = api.wallet.createAddress(seed)
+      const privateKey = api.wallet.createPrivateKey(seed)
+      const walletId = 1
+
+      const to = process.env.TO_ETH_ADDRESS
+      const value = (Math.random() * 1000).toFixed()
+      let events = 0
+
+      const end = once(done)
+
+      emitter.on('error', function (err) {
+        end(err)
+      })
+      emitter.on('wallet-error', function (err) {
+        end(new Error(err.message))
+      })
+      emitter.on('wallet-state-changed', function (data) {
+        try {
+          const { transactions } = data[walletId].addresses[address]
+          if (!transactions) {
+            return
+          }
+          events += 1
+          transactions.should.have.length(1)
+          const { transaction, receipt, meta } = transactions[0]
+          transaction.should.have.property('from', address)
+          transaction.should.have.property('hash').that.is.a('string')
+          transaction.should.have.property('to', to)
+          transaction.should.have.property('value', value)
+          if (events === 2) {
+            transaction.should.have.property('blockHash').that.is.a('string')
+            transaction.should.have.property('blockNumber').that.is.a('number')
+            receipt.should.have.property('blockHash', transaction.blockHash)
+            receipt.should.have.property('blockNumber', transaction.blockNumber)
+            receipt.should.have.property('from', address.toLowerCase())
+            receipt.should.have.property('logs').that.is.an('array')
+            receipt.should.have.property('status').that.is.true
+            receipt.should.have.property('to', to.toLowerCase())
+            receipt.should.have.property('transactionHash').that.is.a('string')
+            meta.should.deep.equal({ contractCallFailed: false })
+            end()
+          } else if (events > 2) {
+            end(new Error('Test should have never reached here'))
+          }
+        } catch (err) {
+          end(err)
+        }
+      })
+
+      emitter.emit('open-wallets', {
+        walletIds: [walletId],
+        activeWallet: walletId,
+        address
+      })
+
+      const transactionObject = {
+        from: address,
+        to,
+        value,
+        gas: 21000
+        // gasPrice: '2000000000'
+      }
+      api.wallet.sendCoin(privateKey, transactionObject).catch(end)
+    })
   })
 
   describe('Qtum', function () {
@@ -190,6 +275,85 @@ describe('Core E2E', function () {
       })
 
       emitter.emit('open-wallets', { activeWallet: walletId, address })
+    })
+
+    it('should send QTUM and emit a wallet event', function (done) {
+      this.timeout(0)
+
+      const core = createCore()
+      const config = {
+        chainId: 'test',
+        chainType: 'qtum',
+        explorerUrl: process.env.QTUMTEST_EXPLORER,
+        nodeUrl: process.env.QTUMTEST_NODE
+      }
+      const { api, emitter } = core.start(config)
+
+      const mnemonic = process.env.MNEMONIC
+      const seed = bip39.mnemonicToSeedHex(mnemonic).toString('hex')
+      const address = api.wallet.createAddress(seed)
+      const privateKey = api.wallet.createPrivateKey(seed)
+      const walletId = 1
+
+      const to = process.env.TO_QTUM_ADDRESS
+      const value = (Math.random() * 1000).toFixed()
+      let events = 0
+
+      const end = once(done)
+
+      emitter.on('error', function (err) {
+        end(err)
+      })
+      emitter.on('wallet-error', function (err) {
+        end(new Error(err.message))
+      })
+      emitter.on('wallet-state-changed', function (data) {
+        try {
+          const { transactions } = data[walletId].addresses[address]
+          if (!transactions) {
+            return
+          }
+          debug('Transaction received %J', transactions)
+          events += 1
+          transactions.should.have.length(1)
+          const { transaction, receipt, meta } = transactions[0]
+          transaction.should.have.property('from', address)
+          transaction.should.have.property('hash').that.is.a('string')
+          transaction.should.have.property('to', to)
+          transaction.should.have.property('value', value)
+          if (events === 2) {
+            transaction.should.have.property('blockHash').that.is.a('string')
+            transaction.should.have.property('blockNumber').that.is.a('number')
+            receipt.should.have.property('blockHash', transaction.blockHash)
+            receipt.should.have.property('blockNumber', transaction.blockNumber)
+            receipt.should.have.property('from', address)
+            receipt.should.have.property('logs').that.is.an('array')
+            receipt.should.have.property('status').that.is.true
+            receipt.should.have.property('to', to)
+            receipt.should.have.property('transactionHash').that.is.a('string')
+            meta.should.deep.equal({ contractCallFailed: false })
+            end()
+          } else if (events > 2) {
+            end(new Error('Test should have never reached here'))
+          }
+        } catch (err) {
+          end(err)
+        }
+      })
+
+      emitter.emit('open-wallets', {
+        walletIds: [walletId],
+        activeWallet: walletId,
+        address
+      })
+
+      const transactionObject = {
+        from: address,
+        to,
+        value
+        // feeRate: 40?
+      }
+      api.wallet.sendCoin(privateKey, transactionObject).catch(end)
     })
   })
 })
